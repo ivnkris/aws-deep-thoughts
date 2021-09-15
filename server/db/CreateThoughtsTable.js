9af3dd940f62cf7b6a1fff525ed239eceeeaa4f7
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-09-15 19:26:21 +0100

    added seed data

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -22,3 +22,17 @@ const params = {
     WriteCapacityUnits: 10,
   },
 };
+
+dynamodb.createTable(params, (err, data) => {
+  if (err) {
+    console.error(
+      "Unable to create table. Error: ",
+      JSON.stringify(err, null, 2)
+    );
+  } else {
+    console.log(
+      "Created table. Table description: ",
+      JSON.stringify(data, null, 2)
+    );
+  }
+});
