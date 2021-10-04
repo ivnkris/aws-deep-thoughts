ec1bfd93b2cda4cd4a4a01c3eadf1f3df294affa
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-10-04 11:13:46 +0100

    added get route to query thoughts from a specific user

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -24,4 +24,31 @@ router.get("/users", (req, res) => {
   });
 });
 
+router.get("/users/:username", (req, res) => {
+  console.log(`Querying for thought(s) from ${req.params.username}.`);
+  const params = {
+    TableName: table,
+    KeyConditionExpression: "#un = :user",
+    ExpressionAttributeNames: {
+      "#un": "username",
+      "#ca": "createdAt",
+      "#th": "thought",
+    },
+    ExpressionAttributeValues: {
+      ":user": req.params.username,
+    },
+    ProjectionExpression: "#th, #ca",
+    ScanIndexForward: false,
+  };
+  dynamodb.query(params, (err, data) => {
+    if (err) {
+      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+      res.status(500).json(err); // an error occurred
+    } else {
+      console.log("Query succeeded.");
+      res.json(data.Items);
+    }
+  });
+});
+
 module.exports = router;
