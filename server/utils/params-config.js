3b40881f7c3d53368acc701e2fa1aad08e67e3ae
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-10-17 11:05:45 +0100

    added image upload route to server

---
+++ server/utils/params-config.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require("uuid");
+
+const params = (fileName) => {
+  const myFile = fileName.originalname.split(".");
+  const fileType = myFile[myFile.length - 1];
+
+  const imageParams = {
+    Bucket: "user-images-dcdce851-bd20-4ea0-b242-610cc621941d",
+    Key: `${uuidv4()}.${fileType}`,
+    Body: fileName.buffer,
+  };
+
+  return imageParams;
+};
+
+module.exports = params;
